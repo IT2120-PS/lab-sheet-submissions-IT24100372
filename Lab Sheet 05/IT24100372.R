7bf47f28948257a886c2d6a789add57e8cda8e15
IT24100372 <IT24100372@MY.SLIIT.LK>
2025-08-26 16:17:52 +0530

    Add files via upload

---
+++ Lab Sheet 05/IT24100372.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\IT24100372\\Desktop\\New folder")
+
+Delivery_Times <- read.table("Exercise - Lab 05.txt", header = TRUE)
+
+Delivery_Times$`Delivery_Time_.minutes.` <- as.numeric(Delivery_Times$`Delivery_Time_.minutes.`)
+
+hist(Delivery_Times$`Delivery_Time_.minutes.`, 
+     breaks = seq(20, 70, by = 5), 
+     right = TRUE, 
+     col = "lightblue", 
+     main = "Histogram of Delivery Times", 
+     xlab = "Delivery Time", 
+     ylab = "Frequency")
+
+
+plot(seq(22.5, 67.5, by = 5), cf, type = "o", col = "blue", 
+     xlab = "Delivery Time", ylab = "Cumulative Frequency", 
+     main = "Cumulative Frequency Polygon (Ogive)")
